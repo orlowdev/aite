72cd3e73c22f9c0bd3068e0b314fb1e2698077fe
zumazu <zumazu.dev@gmail.com>
2017-02-02 09:27:35 +1000

    Add stub mainpage available at '/'

--- src/urls.py
+++ src/urls.py
@@ -18,8 +18,11 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
+from . import views
+
 
 urlpatterns = [
+    url(r'^$', views.index, name="index"),
     url(r'^admin/', admin.site.urls),
     url(r'^posts/', include("posts.urls", namespace="posts")),
 ]
