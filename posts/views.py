9c2a22d30dadfba22c3a4f44e6879fb2fc40518f
zumazu <zumazu.dev@gmail.com>
2017-02-04 12:04:46 +1000

    Add reply logic

--- posts/views.py
+++ posts/views.py
@@ -66,15 +66,26 @@ def post_detail(request, slug=None):
         content_type = ContentType.objects.get(model=c_type)
         obj_id = form.cleaned_data.get("object_id")
         content = form.cleaned_data.get("content")
+        parent_object = None
+        try:
+            parent_id = int(request.POST.get("parent_id"))
+        except ValueError:
+            parent_id = None
+
+        if parent_id:
+            parent_queryset = Comment.objects.filter(id=parent_id)
+            if parent_queryset.exists() and parent_queryset.count() == 1:
+                parent_object = parent_queryset.first()
+
         new_comment, created = Comment.objects.get_or_create(
             user=request.user,
             content_type=content_type,
             object_id=obj_id,
             content=content,
+            parent_id=parent_object,
         )
 
         return HttpResponseRedirect(new_comment.content_object.get_absolute_url())
-
 
     return render(request, "detail.html", {
         "post": post,
