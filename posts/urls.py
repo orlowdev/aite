52ce0a841c20bd31662025cb5e7da60d8e06efad
zumazu <orlov@factorov.ru>
2017-02-07 18:58:47 +1000

    Add top navbar menu

--- posts/urls.py
+++ posts/urls.py
@@ -3,10 +3,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list),
-
-    url(r'^create/$', views.post_create),
+    url(r'^$', views.post_list, name="list"),
+    url(r'^create/$', views.post_create, name="create"),
     url(r'^(?P<slug>[\w-]+)/$', views.post_detail, name="detail"),
     url(r'^(?P<slug>[\w-]+)/edit/$', views.post_update, name="update"),
-    url(r'^(?P<slug>[\w-]+)/delete/$', views.post_delete),
+    url(r'^(?P<slug>[\w-]+)/delete/$', views.post_delete, name="delete"),
 ]
