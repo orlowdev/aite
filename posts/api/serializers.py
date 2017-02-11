5b638481574eadb83fa21bf09c8dd8e208473911
zumazu <orlov@factorov.ru>
2017-02-11 20:43:38 +1000

    Add API hyperlinks

--- posts/api/serializers.py
+++ posts/api/serializers.py
@@ -1,9 +1,22 @@
 """
 Serializer class for Post model
 """
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import (
+    ModelSerializer,
+    HyperlinkedIdentityField,
+)
 
 from posts.models import Post
+
+post_detail_url = HyperlinkedIdentityField(
+    view_name='api-posts:detail',
+    lookup_field='slug',
+)
+
+post_delete_url = HyperlinkedIdentityField(
+    view_name='api-posts:delete',
+    lookup_field='slug',
+)
 
 
 # Provides serialized Post creation
@@ -21,27 +34,31 @@ class PostCreateUpdateSerializer(ModelSerializer):
 
 # Provides serialized single Post data
 class PostDetailSerializer(ModelSerializer):
+    url = post_detail_url
+    delete_url = post_delete_url
 
     class Meta:
         model = Post
         fields = [
             "id",
+            "url",
             "publication_date",
-            "slug",
             "title",
             "content",
+            "delete_url",
         ]
 
 
 # Provides serialized Post list data
 class PostListSerializer(ModelSerializer):
+    url = post_detail_url
 
     class Meta:
         model = Post
         fields = [
             "id",
             "user",
+            "url",
             "publication_date",
-            "slug",
             "title",
         ]
