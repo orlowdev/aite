d224a1ba9cef8d4ad94eb9603cd112c089b791b0
zumazu <orlov@factorov.ru>
2017-02-18 21:08:23 +1000

    Add proof-pages

--- src/views.py
+++ src/views.py
@@ -1,6 +1,11 @@
 # Main page view
 from django.shortcuts import render
 
+from posts.models import Post
+
 
 def index(request):
-    return render(request, "main/index.html", {})
+    posts = Post.objects.all()[:3]
+    return render(request, "main/index.html", {
+        'posts': posts,
+    })
