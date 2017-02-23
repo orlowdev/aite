988eb28527754b24098e94c62701cfddab1dc59a
zumazu <orlov@factorov.ru>
2017-02-23 20:01:37 +1000

    Add bug report and feedback to admin

--- contact_forms/admin.py
+++ contact_forms/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 
-from contact_forms.models import SimpleContact
+from contact_forms.models import BugReport, Feedback, SimpleContact
 
 
-class SimpleContactAdmin(admin.ModelAdmin):
+class BugReportAdmin(admin.ModelAdmin):
     list_display = [
         '__str__',
         'creation_date',
@@ -11,6 +11,29 @@ class SimpleContactAdmin(admin.ModelAdmin):
     ]
 
     class Meta:
+        model = BugReport
+
+
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = [
+        '__str__',
+        'creation_date',
+        'fix_commit',
+    ]
+
+    class Meta:
+        model = Feedback
+
+
+class SimpleContactAdmin(admin.ModelAdmin):
+    list_display = [
+        '__str__',
+        'creation_date',
+    ]
+
+    class Meta:
         model = SimpleContact
 
+admin.site.register(BugReport)
+admin.site.register(Feedback)
 admin.site.register(SimpleContact, SimpleContactAdmin)
