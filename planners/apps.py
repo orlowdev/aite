7567c76bf3426650e61b1b25d023a397d7c19b5f
zumazu <orlov@factorov.ru>
2017-02-24 12:55:30 +1000

    Remove dashboard apps

--- planners/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PlannersConfig(AppConfig):
-    name = 'planners'
--- to_do_lists/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ToDoListsConfig(AppConfig):
-    name = 'to_do_lists'
