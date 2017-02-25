5ce634173e4153f4ca6aa719ac0100987985eb1a
zumazu <orlov@factorov.ru>
2017-02-25 15:55:14 +1000

    Enable filtering

--- calendars/api/views.py
+++ calendars/api/views.py
@@ -1,7 +1,6 @@
 from django.db.models import Q
-from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.filters import SearchFilter
 from rest_framework.generics import ListAPIView
-from rest_framework.permissions import AllowAny
 
 from calendars.api.serializers import EventListSerializer
 from calendars.models import Event
@@ -10,18 +9,19 @@ from calendars.models import Event
 class EventListAPIView(ListAPIView):
     queryset = Event.objects.all()
     serializer_class = EventListSerializer
-    filter_backends = [SearchFilter, OrderingFilter]
+    filter_backends = [SearchFilter]
     search_fields = [
         'title',
     ]
 
     def get_queryset(self, *args, **kwargs):
-        queryset_list = Event.objects.filter(user=self.request.user)
-        query = self.request.GET.get('q')
+        if self.request.user.is_authenticated:
+            queryset_list = Event.objects.filter(user=self.request.user)
+            query = self.request.GET.get('q')
 
-        if query:
-            queryset_list = queryset_list.filter(
-                Q(title__icontains=query)
-            ).distinct()
+            if query:
+                queryset_list = queryset_list.filter(
+                    Q(title__icontains=query)
+                ).distinct()
 
-        return queryset_list
+            return queryset_list
