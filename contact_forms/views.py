a9052d51ea8ab362d371a0f27191461ddeb2d830
zumazu <orlov@factorov.ru>
2017-02-25 12:19:20 +1000

    Autosend email on contact form applied

--- contact_forms/views.py
+++ contact_forms/views.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.contrib import messages
+from django.core.mail import send_mail
 from django.shortcuts import redirect
 
 from contact_forms.forms import SimpleContactForm
@@ -13,5 +15,18 @@ def simple_contact_submission(request):
                          extra_tags="html_safe")
 
         form.save()
+        subject = 'Contact form applied'
+        from_email = settings.EMAIL_HOST_USER
+        to_email = [from_email]
+        contact_message = '{} contacted via simple form'.format(
+            form.cleaned_data.get('email'),
+        )
+        send_mail(
+            subject,
+            contact_message,
+            from_email,
+            to_email,
+            fail_silently=False
+        )
 
     return redirect('index')
