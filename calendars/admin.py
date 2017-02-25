439d845ca58dbc75b03e9c90ab0457e96ff212cc
zumazu <orlov@factorov.ru>
2017-02-25 15:21:17 +1000

    Add calendar model

--- calendars/admin.py
+++ calendars/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from calendars.models import UserCalendar
+
+admin.site.register(UserCalendar)
--- calendars/models.py
+++ calendars/models.py
@@ -1,3 +1,23 @@
+import datetime
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class UserCalendar(models.Model):
+    user = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL,
+        default=1,
+    )
+
+    title = models.CharField(
+        max_length=255,
+    )
+
+    start = models.DateTimeField()
+    end = models.DateTimeField(
+        blank=True,
+        null=True,
+    )
+
+    def __str__(self):
+        return "{} at {}".format(self.title, datetime.datetime.strftime(self.start, "%Y-%m-%dT%X"))
