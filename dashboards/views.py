fd928af3d5363e2c6bcd1f68ff93d9bd2863e369
zumazu <orlov@factorov.ru>
2017-02-28 20:50:14 +1000

    Apply form for creating events

--- dashboards/views.py
+++ dashboards/views.py
@@ -1,12 +1,20 @@
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 from calendars.forms import CreateOrUpdateEventForm
+from posts import options
 
 
 @login_required
 def dashboard_main(request):
-    event_form = CreateOrUpdateEventForm(user=request.user)
+    event_form = CreateOrUpdateEventForm(request.POST or None, user=request.user)
+    if event_form.is_valid():
+        event_form.save()
+        messages.success(request, "<a href='#'>Successfully created</a>", extra_tags=options.ALLOW_FLASH_REDIRECT)
+
+        return HttpResponseRedirect('dashboard')
 
     return render(request, 'user_dashboard.html', {
         'event_form': event_form
