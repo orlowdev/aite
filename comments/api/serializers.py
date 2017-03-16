c2e2b2431d271785a3575624eb985640ca9b502b
zumazu <orlov@factorov.ru>
2017-03-16 22:16:52 +1000

    Optimize comment serialization

--- comments/api/serializers.py
+++ comments/api/serializers.py
@@ -1,12 +1,62 @@
 from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.models import ContentType
 from rest_framework.exceptions import ValidationError
-from rest_framework.serializers import ModelSerializer, SerializerMethodField
+from rest_framework.serializers import ModelSerializer, SerializerMethodField, CharField, SlugField, IntegerField
 
 from accounts.api.serializers import UserDetailSerializer
 from comments.models import Comment
 
 User = get_user_model()
+
+
+class CommentCreateSerializer(ModelSerializer):
+    type = CharField(required=False, write_only=True)
+    slug = SlugField(write_only=True)
+    parent_id = IntegerField(required=False)
+
+    class Meta:
+        model = Comment
+        fields = [
+            "id",
+            "type",
+            "slug",
+            "parent_id",
+            "created_at",
+            "content",
+        ]
+
+    def validate(self, data):
+        model_type = data.get("type", "post")
+        model_qs = ContentType.objects.filter(model=model_type)
+        if not model_qs.exists() or model_qs.count() != 1:
+            raise ValidationError("Invalid content type")
+
+        SomeModel = model_qs.first().model_class()
+        slug = data.get("slug")
+        obj_qs = SomeModel.objects.filter(slug=slug)
+        if not obj_qs.exists() or obj_qs.count() != 1:
+            raise ValidationError("Content type and slug do not match")
+        parent_id = data.get("parent_id")
+        if parent_id:
+            parent_qs = Comment.objects.filter(id=parent_id)
+            if not parent_qs.exists() or parent_qs.count() != 1:
+                raise ValidationError("This is not a valid parent for this content")
+
+        return data
+
+    def create(self, validated_data):
+        content = validated_data.get("content")
+        user = self.context["user"]
+        model_type = validated_data.get("type", "post")
+        slug = validated_data.get("slug")
+        parent_id = validated_data.get("parent_id")
+        parent_obj = Comment.objects.filter(id=parent_id).first() if parent_id else None
+
+        comment = Comment.objects.create_by_model_type(
+            model_type, slug, content, user, parent_obj
+        )
+
+        return comment
 
 
 def create_comment_serializer(model_type='post', slug=None, parent_id=None, user=None):
--- comments/api/views.py
+++ comments/api/views.py
@@ -6,25 +6,19 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 
 from comments.api.serializers import (CommentDetailSerializer,
                                       CommentEditSerializer, CommentSerializer,
-                                      create_comment_serializer)
+                                      create_comment_serializer, CommentCreateSerializer)
 from comments.models import Comment
 from posts.api.pagination import PostPageNumberPagination
 
 
 class CommentCreateAPIView(CreateAPIView):
     queryset = Comment.objects.all()
+    serializer_class = CommentCreateSerializer
 
-    def get_serializer_class(self):
-        model_type = self.request.GET.get("type")
-        slug = self.request.GET.get("slug")
-        parent_id = self.request.GET.get("parent_id", None)
-
-        return create_comment_serializer(
-            model_type=model_type,
-            slug=slug,
-            parent_id=parent_id,
-            user=self.request.user,
-        )
+    def get_serializer_context(self):
+        context = super(CommentCreateAPIView, self).get_serializer_context()
+        context['user'] = self.request.user
+        return context
 
 
 class CommentDetailAPIView(RetrieveAPIView):
