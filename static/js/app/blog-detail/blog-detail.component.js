19620acf498edcaad5903a3d0915113d92eb8b29
zumazu <orlov@factorov.ru>
2017-03-16 21:45:30 +1000

    Unite comments back end and front end

--- static/js/app/blog-detail/blog-detail.component.js
+++ static/js/app/blog-detail/blog-detail.component.js
@@ -3,40 +3,55 @@
 angular.module('blogDetail').
 component('blogDetail', {
     templateUrl: "/api/templates/blog-detail.html",
-    controller: function(Post, $http, $location, $routeParams, $scope) {
-        Post.get({"slug": $routeParams.slug}, function (data) {
+    controller: function(Post, $cookies, $http, $location, $routeParams, $scope) {
+        var slug = $routeParams.slug;
+
+    	Post.get({"slug": slug}, function (data) {
             $scope.post = data;
+            $scope.comments = data.comments;
         });
-        // Post.query(function (data) {
-        // 	$scope.notFound = true
-        // 	$scope.comments = []
-        //
-        // 	angular.forEach(data, function (post) {
-        // 		if (post.id == $routeParams.id) {
-        // 			$scope.notFound = false
-        // 			$scope.post = post
-        // 			if (post.comments) {
-        // 				$scope.comments = post.comments
-        // 			}
-        // 			resetReply()
-        // 		}
-        // 	})
-        // })
 
         $scope.deleteComment = function(comment) {
             $scope.$apply($scope.comments.splice(comment, 1))
         };
 
         $scope.addReply = function() {
-            console.log($scope.reply);
-            $scope.comments.push($scope.reply);
-            resetReply();
+        	var token = $cookies.get("token");
+
+        	if (token) {
+        		var request = {
+					method: "POST",
+					url: "http://127.0.0.1:8000/api/comments/create/?slug=" + slug + "&type=post",
+					data: {
+						content: $scope.reply.content,
+					},
+					headers: {
+						authorization: "JWT " + token,
+					},
+				};
+
+        		$http(request).then(
+        		    function (response) {
+                        $scope.comments.push($scope.reply);
+                        resetReply();
+                    }, function (response) {
+                        console.log(response);
+                    }
+                );
+
+				console.log($scope.reply);
+				resetReply();
+			} else {
+        		console.log("no token");
+			}
+
+
         };
 
         function resetReply() {
             $scope.reply = {
-                "id": $scope.comments.length + 1,
-                "text": "",
+                id: $scope.comments.length + 1,
+                content: "",
             }
         }
 
